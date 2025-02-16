06c899d28e9df914b0acc95c884a3d9227622a25
Sonnet Xu <59452214+sonnetx@users.noreply.github.com>
2025-02-15 16:48:15 -0800

    integrate pipeline

--- report/controller.py
+++ report/controller.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 from report import ReportData, Report
-from ..basic_pipeline import EmissionsResponse, TrashAnalyzer
+from basic_pipeline import EmissionsResponse, TrashAnalyzer
+
+import os
 
 
 app = Flask(__name__)
@@ -9,8 +11,17 @@ app = Flask(__name__)
 def get_data():
     
     
-    reportData = ReportData(numTrash=40, numCompost=5, numRecycle=15, recycleNames=["Plastic", "Paper"], compostNames=["Apple Core"], recycleSavings=20, compostSavings=10)
-    report = Report(report_data=reportData)
+    # reportData = ReportData(numTrash=40, numCompost=5, numRecycle=15, recycleNames=["Plastic", "Paper"], compostNames=["Apple Core"], recycleSavings=20, compostSavings=10)
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    perplexity_api_key = os.getenv("PERPLEXITY_API_KEY")
+    
+    analyzer = TrashAnalyzer(openai_api_key, perplexity_api_key)
+    
+    before_image_path = "sample-images/notrash.jpg"
+    after_image_path = "sample-images/IMG_6703.jpg"
+    report_data = analyzer.analyze_trash(before_image_path, after_image_path)
+
+    report = Report(report_data=report_data)
     return jsonify(report.to_dict())  # Send as JSON
 
 if __name__ == '__main__':
